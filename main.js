7d1c9be6576f2af37273c93402eab6aa125d0a6c
5GirlCoder <73702929+5GirlCoder@users.noreply.github.com>
2021-03-06 11:24:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var canvas = new fabric.Canvas("myCanvas");
+var Player_x = 10;
+var Player_y = 10;
+var blockWidth = 30;
+var blockHeight = 30;
+var playerObject = "";
+var blockObject = "";
+function playerUpdate()
+{
+    fabric.Image.fromURL("player.png", function(Img)
+    {
+        playerObject = Img;
+        playerObject.scaleToWidth(150);
+        playerObject.scaleToHeight(140);
+        playerObject.set({
+            top : Player_y,
+            left : Player_x
+        });
+        canvas.add(playerObject);
+    });
+}
+function newImage(get_image)
+{
+    fabric.ImagefromURL(get_image, function(Img)
+    {
+        blockObject = Img;
+        blockObject.scaleToWidth(blockWidth);
+        blockObject.scaleToHeight(blockHeight);
+        blockObject.set({
+            top : Player_y,
+            left : Player_x
+        });
+        canvas.add(blockObject);
+    });
+}
